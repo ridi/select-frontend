a04b77b31bb14a4ca86b82d17c6a6bc7814b90d1
이준우 <junwoo.lee@ridi.com>
2019-03-19 12:15:32 +0900

    공통으로 처리하는 페이지네이션 params 후처리 메서드로 묶음 처리.

--- src/app/services/searchResult/sagas.ts
+++ src/app/services/searchResult/sagas.ts
@@ -5,6 +5,7 @@ import { FetchErrorFlag } from 'app/constants';
 import { Actions as BookActions } from 'app/services/book';
 import { Actions } from 'app/services/searchResult';
 import { requestSearchResult, SearchResultReponse } from 'app/services/searchResult/requests';
+import { paginationErrorCallback } from 'app/utils/pageParamsErrorHelper';
 import { updateQueryStringParam } from 'app/utils/request';
 import toast from 'app/utils/toast';
 
@@ -30,14 +31,7 @@ export function* watchQueryKeyword() {
 export function* watchQueryKeywordFailure() {
   while (true) {
     const { payload: { page, error } }: ReturnType<typeof Actions.queryKeywordFailure> = yield take(Actions.queryKeywordFailure.getType());
-    if (error === FetchErrorFlag.UNEXPECTED_PAGE_PARAMS) {
-      toast.failureMessage('없는 페이지입니다. 첫번째 페이지로 이동합니다.');
-      history.replace(`?${updateQueryStringParam('page', 1)}`);
-    } else if (page === 1) {
-      toast.failureMessage('없는 페이지입니다. 다시 시도해주세요.');
-    } else if (!page) {
-      toast.failureMessage();
-    }
+    paginationErrorCallback(error, page);
   }
 }
 
