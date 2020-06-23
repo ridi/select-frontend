4e10c00089102ad76b0018cd548397fca614277f
shrimp1box <nnyamm@ridi.com>
2020-06-23 12:07:10 +0900

    rgba util 파라미터 수정

--- src/app/utils/colors.ts
+++ src/app/utils/colors.ts
@@ -1,4 +1,12 @@
-export const rgba = (rgbHex: string, alphaPercentage: number) =>
-  `${rgbHex}${Math.round((alphaPercentage * 255) / 100)
+/**
+ * rgb hex 값과 alpha 값을 조합하여 rgba hex 값을 반환
+ * @param rgbHex rgb hex값
+ * @param alpha 0 ~ 1 사이의 알파값
+ * @return rgbaHex
+ */
+export const rgba = (rgbHex: string, alpha: number): string => {
+  const alphaHex = Math.round(alpha * 255)
     .toString(16)
-    .padStart(2, '0')}`;
+    .padStart(2, '0');
+  return `${rgbHex}${alphaHex}`;
+};
